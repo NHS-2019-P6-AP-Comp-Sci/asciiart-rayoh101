a3f6877cac3d46ecf368aa81279c983c575aae6b
s-ohr <s-ohr@53S50023202.it.bsd405.org>
2019-09-20 13:42:05 -0700

    Turning in project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -18,10 +18,61 @@
 
 public class ASCIIArt {
 	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+		System.out.println("This is my ASCII ART which says \"Hi\"");
+		Htop();
+		Hbottom();
+		Itop();
+		Imiddle(); 
+		Imiddle(); 
+		Imiddle(); 
+		Imiddle(); 
+		Imiddle(); 
+		Ibottom();
+		
 	}
+	
+	public static void Htop() {
+		System.out.println("\t| \\==\\\t\t|\\==\\");
+		System.out.println("\t|   \\__\\\t| \\ __\\");
+		System.out.println("\t|   |\t |\t|  |    | ");
+		System.out.println("\t|   |\t |______ \\ |    |");
+		System.out.println("\t|   |\t |_______ \\|\t|");
+		System.out.println("\t|   |\t  ________\t|");
+	}
+		
+	public static void Hbottom() {
+		Hpart();
+		Hpart();
+		Hpart();
+		System.out.println("         \\  |     |     \\  |    | ");
+		System.out.println("           \\|_____|      \\ |____| ");
+	}
+		
+		
+		
+	public static void Hpart() {
+		System.out.println("        |   |     |     |  |    |");
+	}
+	
+	public static void Itop() {
+		System.out.println(" _______________________");
+		System.out.println("| \\_____________________\\");
+		System.out.println(" \\|_______      ________|");
+	}
+	
+	public static void Imiddle() {
+		System.out.println("        | |     | ");
+	}
+	public static void Ibottom() {
+		System.out.println(" _______| |     |_______");
+		System.out.println("|\\______ \\|     |_______\\");
+		System.out.println(" \\|______________________|");
+	}
+	
+	
+	
+	
 }
+
+
+		
